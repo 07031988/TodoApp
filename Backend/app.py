86edcc2e985ff220224b0fa129810cf28dbf7c99
07031988 <mohammedguennoun@mohammeds-MacBook-Pro.local>
2020-07-05 18:01:15 +0200

    created add user api call

---
+++ Backend/app.py
@@ -0,0 +1,11 @@
+from flask import Blueprint
+from flask_restful import Api
+from resources.User import User
+from resources.Register import Register
+
+api_bp = Blueprint('api', __name__)
+api = Api(api_bp)
+
+# Route
+api.add_resource(User, '/User')
+api.add_resource(Register, '/Register')
